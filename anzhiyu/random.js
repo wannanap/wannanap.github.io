8b5c2c99c0db8e9fe0fa6976000e9c497f5e4dc5
WisdomX <2546469725@qq.com>
2025-04-07 17:41:23 +0800

    Site updated: 2025-04-07 17:41:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/19/AppleID美区注册/","2023/05/09/博客搭建/","2023/05/18/搭建图床/"];function toRandomPost(){
+var posts=["2023/05/19/AppleID美区注册/","2023/05/18/搭建图床/","2023/05/09/博客搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
