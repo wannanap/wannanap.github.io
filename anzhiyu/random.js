5bb57eb822f2a6faa59bb552113b1db1ac55d526
WisdomX <2546469725@qq.com>
2025-04-08 16:51:57 +0800

    Site updated: 2025-04-08 16:51:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/19/AppleID美区注册/","2023/05/09/博客搭建/","2025/04/08/GitHubPages自定义域名/","2023/05/18/图床搭建/","2025/04/08/设置cdn加速/"];function toRandomPost(){
+var posts=["2023/05/19/AppleID美区注册/","2025/04/08/GitHubPages自定义域名/","2023/05/09/博客搭建/","2025/04/08/设置cdn加速/","2023/05/18/图床搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
